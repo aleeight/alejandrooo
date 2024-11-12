7515ac22c21d7e6c54bca8c2e9e9ad9aea9985af
I_can <i_cant_wake_up@outlook.com>
2024-11-12 00:21:10 -0300

    txttitulo = findViewById(R.id.txtTitulo);         txtCuerpo = findViewById(R.id.txtCuerpo);         txtNotaEliminar = findViewById(R.id.txtNotaEliminar);         btnAgregarNota = findViewById(R.id.btnAgregarNota);         btnEliminarNota = findViewById(R.id.btnEliminarNota);         ListaNotas = findViewById(R.id.listnota);

--- app/src/main/java/com/example/practicandocertamen/MainActivity.java
+++ app/src/main/java/com/example/practicandocertamen/MainActivity.java
@@ -6,15 +6,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     //... variables que se utilizaron   =) //
-    EditText txttitulo, txtcuerpo, txtnotaEliminar;
+    EditText txttitulo,txtCuerpo,txtNotaEliminar;
     Button btnAgregarNota,btnEliminarNota;
     ListView ListaNotas;
+
+    // aqui va un arreglo que va a almacenar todas las notas que se vallan almacenando //
+    ArrayList<Nota> notas = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //aqui tengo el codigo de todas los elementso de la interfaz//
+
+        txttitulo = findViewById(R.id.txtTitulo);
+        txtCuerpo = findViewById(R.id.txtCuerpo);
+        txtNotaEliminar = findViewById(R.id.txtNotaEliminar);
+        btnAgregarNota = findViewById(R.id.btnAgregarNota);
+        btnEliminarNota = findViewById(R.id.btnEliminarNota);
+        ListaNotas = findViewById(R.id.listnota);
+
+
     }
 }
